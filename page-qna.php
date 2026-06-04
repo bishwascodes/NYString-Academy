b4322f7dac26555a9394265eabde35d2ace25c24
Bishwas Thapa <bishwas360@gmail.com>
2026-06-03 22:20:44 -0600

    Added QNA Page template

---
+++ page-qna.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: Q&A Page */ ?>
+<?php get_header(); ?>
+
+<div class="rs-breadcrumbs bg7 breadcrumbs-overlay">
+    <div class="breadcrumbs-inner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1 class="page-title"><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: About Us Page */ ?>
+<?php get_header(); ?>
+
+<div class="rs-breadcrumbs bg7 breadcrumbs-overlay">
+    <div class="breadcrumbs-inner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1 class="page-title"><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
