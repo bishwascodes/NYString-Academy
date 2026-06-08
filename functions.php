ce8f95feb34535a4dff51fcc173eb77e10e16aa6
Bishwas Thapa <bishwas360@gmail.com>
2026-06-07 20:29:17 -0600

    Shuffled the js and css scripts in right order

--- functions.php
+++ functions.php
@@ -6,36 +6,70 @@
  */
 add_action( 'wp_enqueue_scripts', 'nystringacademy_enqueue_styles' );
 function nystringacademy_enqueue_styles() {
-        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'child-style',
-            get_stylesheet_directory_uri() . '/style.css',
-            array('parent-style')
-        );
+         // Bootstrap CSS
         wp_enqueue_style(
-            'slick-style',
-            get_stylesheet_directory_uri() . '/css/slick.css',
-            array()
+            'bootstrap-css',
+            '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+            array(),
+            '5.3.3'
         );
+
+         wp_enqueue_style(
+            'fa',
+            '//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+            array(),
+            null
+         );
+
         wp_enqueue_style(
             'animate-style',
             get_stylesheet_directory_uri() . '/css/animate.css',
             array()
         );
+
+        wp_enqueue_style(
+            'animate-style',
+            get_stylesheet_directory_uri() . '/css/owl-carousel.css',
+            array()
+        );
+
+        wp_enqueue_style(
+            'slick-style',
+            get_stylesheet_directory_uri() . '/css/slick.css',
+            array()
+        );
+
+        wp_enqueue_style(
+            'slick-style',
+            get_stylesheet_directory_uri() . '/css/magnific-popup.css',
+            array()
+        );
+
+         wp_enqueue_style(
+            'off-canvas-style',
+            get_stylesheet_directory_uri() . '/css/off-canvas.css',
+            array()
+        );
+
         wp_enqueue_style(
             'rsmenu-main-style',
             get_stylesheet_directory_uri() . '/css/rsmenu-main.css',
             array()
         );
-        wp_enqueue_style(
+
+         wp_enqueue_style(
             'rsmenu-transitions-style',
             get_stylesheet_directory_uri() . '/css/rsmenu-transitions.css',
             array( 'rsmenu-main-style' )
         );
-        wp_enqueue_style(
-            'off-canvas-style',
-            get_stylesheet_directory_uri() . '/css/off-canvas.css',
-            array()
+
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+        
+        wp_enqueue_style( 'child-style',
+            get_stylesheet_directory_uri() . '/style.css',
+            array('parent-style')
         );
+
         wp_enqueue_style(
             'child-custom-style',
             get_stylesheet_directory_uri() . '/css/customstyle.css',
@@ -48,14 +82,12 @@ function nystringacademy_enqueue_styles() {
         );
         
         
-    // Bootstrap CSS
-    wp_enqueue_style(
-        'bootstrap-css',
-        '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-        array(),
-        '5.3.3'
-    );
+   
+   
+   
 
+}
+function load_custom_js() {
     // Bootstrap JS
     wp_enqueue_script(
         'bootstrap-js',
@@ -64,16 +96,7 @@ function nystringacademy_enqueue_styles() {
         '5.3.3',
         true
     );
-    wp_enqueue_style(
-        'fa',
-        '//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
-        array(),
-        null
-    );
-
-}
-function load_custom_js() {
-   
+    
     wp_enqueue_script(
         'owl-js',
         get_stylesheet_directory_uri() . '/js/owl-carousel-min.js',
