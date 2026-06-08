7ee325e65f063f1a83f9245bf0202e05ba5ea962
Bishwas Thapa <bishwas360@gmail.com>
2026-06-07 22:11:12 -0600

    Added only one carousel

--- js/custom.js
+++ js/custom.js
@@ -190,6 +190,25 @@ jQuery(document).ready(function ($) {
         });
     }
 
+    // Instrument Slider
+    if ($('.instrument_slider').length) {
+        $('.instrument_slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: false,
+            autoplaySpeed: 3000,
+            arrows: true,
+            dots: false,
+            infinite: true,
+            speed: 500,
+            prevArrow: '<div class="owl-prev"><svg width="24px" height="24px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="#000000" /></svg></div>',
+            nextArrow: '<div class="owl-next"><svg width="24px" height="24px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z" fill="#000000" /></svg></div>',
+            responsive: [
+                { breakpoint: 980, settings: { slidesToShow: 2, slidesToScroll: 1, arrows: false } },
+                { breakpoint: 767, settings: { slidesToShow: 1, slidesToScroll: 1, arrows: false } }
+            ]
+        });
+    }
 
     // Single Product Slick
     if ($('.single-product').length) {
